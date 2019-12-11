782d01cc6701806e5b3fc93f3ebde899ab011bc3
adrianFarkas <farkasadrian1996@gmail.com>
2019-12-11 12:24:09 +0100

    setup user for auth

---
+++ src/main/java/com/codecool/videoservice/model/VideoAppUser.java
@@ -0,0 +1,35 @@
+package com.codecool.videoservice.model;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class VideoAppUser {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @NotNull
+    private String userName;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+}
---
+++ src/main/java/com/codecool/videoservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.videoservice.repository;
+
+import com.codecool.videoservice.model.VideoAppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<VideoAppUser,String> {
+
+    Optional<VideoAppUser> findByUsername(String username);
+}
