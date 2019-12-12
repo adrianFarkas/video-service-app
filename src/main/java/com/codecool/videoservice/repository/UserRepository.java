54af4258df270c4d568d0f4d8b8f65b32f670d70
adrianFarkas <farkasadrian1996@gmail.com>
2019-12-12 09:28:55 +0100

    remove method because user will verify itself with email

--- src/main/java/com/codecool/videoservice/repository/UserRepository.java
+++ src/main/java/com/codecool/videoservice/repository/UserRepository.java
@@ -7,7 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<VideoAppUser,String> {
 
-    Optional<VideoAppUser> findByUserName(String username);
-
     Optional<VideoAppUser> findByEmail(String email);
 }
