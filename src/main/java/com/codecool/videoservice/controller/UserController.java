e4e2655c00def197263eeb86a30ea8a9b845c035
adrianFarkas <farkasadrian1996@gmail.com>
2020-01-16 21:01:15 +0100

    new route to find specific user

--- src/main/java/com/codecool/videoservice/controller/UserController.java
+++ src/main/java/com/codecool/videoservice/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class UserController {
     public BasicUserInformation getUserData() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return userRepository.getBasicInformationByEmail(auth.getName());
+    }
+
+    @GetMapping("/{id}")
+    public BasicUserInformation getUserData(@PathVariable String id) {
+        return userRepository.getBasicInformationById(id);
     }
 }
