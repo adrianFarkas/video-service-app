d7d67c1eafd21402807294b92c5775c87029fc4b
adrianFarkas <farkasadrian1996@gmail.com>
2020-01-16 16:06:57 +0100

    modified to use the generated filenames

--- src/main/java/com/codecool/videoservice/filestore/FileStore.java
+++ src/main/java/com/codecool/videoservice/filestore/FileStore.java
@@ -7,12 +7,17 @@ import com.codecool.videoservice.model.Video;
 import com.codecool.videoservice.model.user.VideoAppUser;
 import com.codecool.videoservice.repository.VideoRepository;
 import com.codecool.videoservice.service.AuthService;
+import static com.codecool.videoservice.util.FileType.JPG;
+import static com.codecool.videoservice.util.FileType.MP4;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.text.Normalizer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class FileStore {
@@ -33,15 +38,25 @@ public class FileStore {
                      MultipartFile thumbnail, String title, String description) {
 
         VideoAppUser appUser = authService.getAuthenticatedUser();
+        String videoName = generateFileName(title, MP4.getFormat());
+        String thumbName = generateFileName(title, JPG.getFormat());
 
         try {
-            s3.putObject(bucket, createS3FilePath(appUser, video),
-                    video.getInputStream(), getFileMetadata(video));
+            s3.putObject(
+                    bucket,
+                    createS3FilePath(appUser, videoName),
+                    video.getInputStream(),
+                    getFileMetadata(video)
+            );
 
-            s3.putObject(bucket, createS3FilePath(appUser, thumbnail),
-                    thumbnail.getInputStream(), getFileMetadata(thumbnail));
+            s3.putObject(
+                    bucket,
+                    createS3FilePath(appUser, thumbName),
+                    thumbnail.getInputStream(),
+                    getFileMetadata(thumbnail)
+            );
 
-            storeInDatabase(appUser, title, description, video, thumbnail);
+            storeInDatabase(appUser, title, description, videoName, thumbName);
 
         } catch (AmazonServiceException | IOException e) {
             throw new IllegalArgumentException("Failed to store file to s3", e);
@@ -49,14 +64,14 @@ public class FileStore {
     }
 
     private void storeInDatabase(VideoAppUser user, String title, String description,
-                                 MultipartFile video, MultipartFile thumbnail) {
+                                 String videoName, String thumbnailName) {
 
         Video newVideo = Video.builder()
                 .videoAppUser(user)
                 .title(title)
                 .description(description)
-                .videoLink(createUserFilePath(user, video))
-                .thumbNailLink(createUserFilePath(user, thumbnail))
+                .videoLink(createUserFilePath(user, videoName))
+                .thumbNailLink(createUserFilePath(user, thumbnailName))
                 .build();
 
         videoRepository.save(newVideo);
@@ -69,12 +84,21 @@ public class FileStore {
         return metadata;
     }
 
-    private String createS3FilePath(VideoAppUser user, MultipartFile file) {
-        return String.format("%s/%s", user.getId(), file.getOriginalFilename());
+    private String createS3FilePath(VideoAppUser user, String filename) {
+        return String.format("%s/%s", user.getId(), filename);
     }
 
-    private String createUserFilePath(VideoAppUser appUser, MultipartFile file) {
-        return String.format("%s/%s/%s", cloudFront, appUser.getId(), file.getOriginalFilename());
+    private String createUserFilePath(VideoAppUser appUser, String filename) {
+        return String.format("%s/%s/%s", cloudFront, appUser.getId(), filename);
     }
 
+    private String generateFileName(String fromString, String type) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm");
+
+        fromString = Normalizer.normalize(fromString, Normalizer.Form.NFD);
+        fromString = fromString.replaceAll("[^\\p{ASCII}]", "");
+        fromString = String.join("_", fromString.split("[^a-zA-Z0-9]+"));
+
+        return String.format("%s_%s.%s", fromString, dateFormat.format(new Date()), type);
+    }
 }
