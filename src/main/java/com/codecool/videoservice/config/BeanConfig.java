641d2683ad8ef3c1fad0ba6cfaa188f74b26acbe
adrianFarkas <farkasadrian1996@gmail.com>
2020-01-16 16:00:57 +0100

    created bean from Random class

--- src/main/java/com/codecool/videoservice/config/BeanConfig.java
+++ src/main/java/com/codecool/videoservice/config/BeanConfig.java
@@ -5,11 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Random;
+
 @Configuration
 public class BeanConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Random random() {
+        return new Random();
     }
 }
