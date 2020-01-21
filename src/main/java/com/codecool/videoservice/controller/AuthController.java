55104c40bcdf58ab48615b22b2157b2a3dade15d
adrianFarkas <farkasadrian1996@gmail.com>
2020-01-21 10:55:56 +0100

    response if user is exists

--- src/main/java/com/codecool/videoservice/controller/AuthController.java
+++ src/main/java/com/codecool/videoservice/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.codecool.videoservice.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -18,21 +19,30 @@ public class AuthController {
 
     @PostMapping("/sign-up")
     public ResponseEntity signUp(@RequestBody VideoAppUser user) {
-        if (authService.isEmailExists(user.getEmail()))
-            return new ResponseEntity<>("Already exists!", HttpStatus.CONFLICT);
         authService.saveUser(user);
         return ResponseEntity.ok("User saved successfully!");
     }
 
     @PostMapping("/sign-in")
     public ResponseEntity signIn(@RequestBody VideoAppUser user, HttpServletResponse response) {
-        authService.authenticate(user, response);
-        return new ResponseEntity(HttpStatus.OK);
+        try {
+            authService.authenticate(user, response);
+        } catch (AuthenticationException e) {
+            return ResponseEntity.ok(false);
+        }
+        return ResponseEntity.ok(true);
     }
 
     @PostMapping("/logout")
     public ResponseEntity signIn(HttpServletResponse response) {
         authService.logout(response);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/exists")
+    public ResponseEntity checkEmailExists(@RequestParam String email) {
+        if (authService.isEmailExists(email))
+            return ResponseEntity.ok(true);
+        return  ResponseEntity.ok(false);
     }
 }
